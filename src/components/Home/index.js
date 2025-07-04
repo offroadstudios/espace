7fba3a2b90e194280133325795e0eafb919fa4b4
Offroad Studios <jawad.liaqat@offroadstudios.com>
2025-07-04 05:03:28 +0500

    Add slider for home sections

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,14 @@
 import React from "react";
 // import { Outlet } from "react-router-dom";
 import { Header } from "../TopBar/Header";
-import About from "./About";
-import About1 from "./About1";
 import Contact from "./Contact";
 import Footer from "./Footer";
-import Interview from "./Interview";
-import Interview1 from "./Interview1";
 import VideoGallery from "./VideoGallery";
 import LandingPage from "./LandingPage";
 import Skills from "./Skills";
 import Testimonial from "./Testimonial";
 import GallerySection from "./GallerySection";
+import HomeSectionsSlider from "./HomeSectionsSlider";
 const index = () => {
   return (
     <>
@@ -20,13 +17,8 @@ const index = () => {
 
       {/* <Outlet /> */}
       <div/>
-      <About />
-      <Interview />
-
-      <About1 />
-<GallerySection/>
-      <Interview1 />
-
+      <HomeSectionsSlider />
+      <GallerySection/>
       <VideoGallery />
 
       <Contact />
