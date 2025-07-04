4956c291377c11902ba07b330b3a26440c90a33e
Offroad Studios <jawad.liaqat@offroadstudios.com>
2025-07-04 05:55:26 +0500

    Remove social links and add WhatsApp contact button

--- src/data/data.js
+++ src/data/data.js
@@ -8,16 +8,9 @@ import GalleryImg6 from "../asset/gallery/image6.jpg";
 import GalleryImg7 from "../asset/gallery/image7.jpg";
 import GalleryImg8 from "../asset/gallery/image8.jpg";
 import QuoteImg from "../asset/testimonial/quote.svg";
-import { GrFacebookOption } from "react-icons/gr";
 import { IoMdArrowForward } from "react-icons/io";
 import { FiSend } from "react-icons/fi";
 import { FaPlay, FaMapMarkerAlt, FaPhoneAlt, FaEnvelope } from "react-icons/fa";
-import {
-  IoLogoInstagram,
-  IoLogoPinterest,
-  IoLogoTwitter,
-  IoLogoYoutube,
-} from "react-icons/io";
 
 export const headerData = {};
 
@@ -30,10 +23,6 @@ export const navData = {
   ],
 };
 
-export const socialData = [
-  { href: "/", icon: <IoLogoInstagram /> },
-  { href: "/", icon: <IoLogoPinterest /> },
-];
 
 export const heroData = {
   title: "I’m Void Sir.",
@@ -57,7 +46,7 @@ export const about1Data = {
   btnIcon: <IoMdArrowForward />,
 };
 export const galleryData = {
-  title: "",
+  title: "Gallery",
   btnText: "View all",
   btnIcon: <IoMdArrowForward />,
   images: [
